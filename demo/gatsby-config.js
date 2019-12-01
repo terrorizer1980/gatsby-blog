4bd4803d5efacd33f36dc9bd8ced82e321034dff
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-01 15:26:50 +0530

    Update gatsby-config.js

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,25 +1,9 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
 module.exports = {
-  plugins: [
-    {
-      resolve: `@nehalist/gatsby-theme-nehalem`,
-      options: { // optional theme options
-        // location to our content
-        contentPath: `content`,
-        // the page manifest
-        manifest: {
-          name: `nehalem - A Gatsby theme`,
-          short_name: `nehalem`,
-          start_url: `/`,
-          background_color: `#a4cbb8`,
-          theme_color: `#a4cbb8`,
-          display: `minimal-ui`,
-          icon: `${__dirname}/assets/nehalist-gatsby.png`
-        },
-        // if archive pages should be generated automatically
-        loadDefaultPages: true,
-        // posts shown on the front page
-        postsPerPage: 5
-      }
-    }
-  ],
+  plugins: [`@nehalist/gatsby-theme-nehalem`],
 };
