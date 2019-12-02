1722754e9295f69f17bf7074578cb4d26f7ec77a
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-02 09:00:18 +0530

    Update gatsby-config.js

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -9,8 +9,29 @@
 
  */
 
-
 module.exports = {
-  plugins: [`@nehalist/gatsby-theme-nehalem`],
+  plugins: [
+    {
+      resolve: `@nehalist/gatsby-theme-nehalem`,
+      options: { // optional theme options
+        // location to our content
+        contentPath: `./content`,
+        // the page manifest
+        manifest: {
+          name: `nehalem - A Gatsby theme`,
+          short_name: `nehalem`,
+          start_url: `/`,
+          background_color: `#a4cbb8`,
+          theme_color: `#a4cbb8`,
+          display: `minimal-ui`,
+          icon: `${__dirname}/assets/nehalist-gatsby.png`
+        },
+        // if archive pages should be generated automatically
+        loadDefaultPages: true,
+        // posts shown on the front page
+        postsPerPage: 5
+      }
+    }
+  ],
 };
 
