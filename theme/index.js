72594ed8876e08339653e42639d4a9544702b287
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-02 10:11:05 +0530

    Update index.js

--- theme/index.js
+++ theme/index.js
@@ -1,1 +1,12 @@
 // boop
+  <script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+</script>
