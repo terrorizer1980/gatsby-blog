29b8e9ca7a7d5cce78f4d1d45f197e191535d3f8
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-02 08:03:50 +0530

    Update gatsby-config.js

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -2,8 +2,25 @@
  * Configure your Gatsby site with this file.
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ 
+ module.exports = {
+  plugins: [`@nehalist/gatsby-theme-nehalem`],
+};
+
  */
 
 module.exports = {
   plugins: [`@nehalist/gatsby-theme-nehalem`],
 };
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-ghost`,
+      options: {
+        apiUrl: `https://gatsby.ghost.io`,
+        contentApiKey: `9cc5c67c358edfdd81455149d0`,
+      },
+    },
+  ],
+}
