691fe633fd857a42163176185248480bb4992d2a
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-02 17:33:10 +0530

    Update index.tsx

--- theme/src/components/comments/index.tsx
+++ theme/src/components/comments/index.tsx
@@ -4,27 +4,6 @@ import React, {FunctionComponent} from "react";
  * Placeholder which is attached under every post. Can be shadowed to
  * quickly integrate comments (like commento, Disqus, ...).
  */
-<div id="disqus_thread"></div>
-<script>
-
-/**
-*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-/*
-var disqus_config = function () {
-this.page.url = https://akhilnaidu.netlify.com;  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = 123456789; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-*/
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = 'https://ourconfig.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-
 
 
 const Comments: FunctionComponent = () => <></>;
