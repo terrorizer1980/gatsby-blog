cdd5646cf707e0ebc00594df0de43a9575bb297c
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-09 23:52:03 +0530

    Update theme.ts

--- theme/src/styles/theme.ts
+++ theme/src/styles/theme.ts
@@ -3,8 +3,8 @@ import {DefaultTheme} from "./default-theme";
 const Theme: DefaultTheme = {
   layout: {
     backgroundColor: `#fafafa`,
-    primaryColor: `#a4cbb8`,
-    linkColor: `#a4cbb8`,
+    primaryColor: `#6fe5ab`,
+    linkColor: `#6fe5ab`,
   },
   breakpoints: {
     xs: `425px`,
@@ -23,7 +23,7 @@ const Theme: DefaultTheme = {
     },
     header: {
       height: `440px`,
-      background: `linear-gradient(-45deg, #44596e, #6fe5ab) repeat scroll 0 0 transparent`,
+      background: `linear-gradient(-45deg, #44596e, #a4cbb8) repeat scroll 0 0 transparent`,
     },
   },
 };
