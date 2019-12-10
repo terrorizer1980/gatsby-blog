05d92f2be28eb60b807f5550d0a2e647dcea474e
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-10 21:23:33 +0530

    Update index.tsx

--- theme/src/components/comments/index.tsx
+++ theme/src/components/comments/index.tsx
@@ -5,6 +5,8 @@ import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
  * Placeholder which is attached under every post. Can be shadowed to
  * quickly integrate comments (like commento, Disqus, ...).
  */
+<div id="commento"></div>
+<script>src="https://commento.leewardslope.com/js/commento.js"></script>
 
  const Comments: FunctionComponent = () => <></> 
 
