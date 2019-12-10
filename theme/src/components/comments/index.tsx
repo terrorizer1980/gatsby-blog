373985a127cd1528891e87ca00f3958a5c58bfe4
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-10 18:16:33 +0530

    Update index.tsx

--- theme/src/components/comments/index.tsx
+++ theme/src/components/comments/index.tsx
@@ -4,12 +4,7 @@ import React, {FunctionComponent} from "react";
  * Placeholder which is attached under every post. Can be shadowed to
  * quickly integrate comments (like commento, Disqus, ...).
  */
-const Comments: FunctionComponent = () => <>
-    shortname="lewardslope"
-    identifier={post.id}
-    title={post.title}
-    url={post.url}
-    category_id={post.category_id}</>;
+const Comments: FunctionComponent = () => <> </>;
 
 export default Comments;
 
