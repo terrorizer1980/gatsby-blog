3cfc828bd938b6030e8bf736457ad59a7c03dd12
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-10 19:41:02 +0530

    Update index.tsx

--- theme/src/components/comments/index.tsx
+++ theme/src/components/comments/index.tsx
@@ -1,13 +1,26 @@
 import React, {FunctionComponent} from "react";
-
+import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
 
 /**
  * Placeholder which is attached under every post. Can be shadowed to
  * quickly integrate comments (like commento, Disqus, ...).
  */
-
-
-const Comments: FunctionComponent = () => <></> 
+const Comments: FunctionComponent = () => {
+  let disqusConfig = {
+    url: `${config.siteUrl+location.pathname}`,
+    identifier: post.id,
+    title: post.title,
+  }
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <CommentCount config={disqusConfig} placeholder={'...'} />
+      /* Post Contents */
+      <Disqus config={disqusConfig} />
+    </>
+  )
+}
+// const Comments: FunctionComponent = () => <></> 
 
 export default Comments;
 
