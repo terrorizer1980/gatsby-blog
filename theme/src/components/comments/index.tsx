fe6c4a5d73e9653eca4ff71b0f78f3b46f350fe0
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-10 18:52:05 +0530

    Update index.tsx

--- theme/src/components/comments/index.tsx
+++ theme/src/components/comments/index.tsx
@@ -5,8 +5,10 @@ import ReactDisqusComments from 'react-disqus-comments';
  * Placeholder which is attached under every post. Can be shadowed to
  * quickly integrate comments (like commento, Disqus, ...).
  */
-const Comments: FunctionComponent = () => <div id="commento"></div>
+<div id="commento"></div>
 <script src="https://cdn.commento.io/js/commento.js"></script>;
+
+const Comments: FunctionComponent = () => <></> 
 
 export default Comments;
 
