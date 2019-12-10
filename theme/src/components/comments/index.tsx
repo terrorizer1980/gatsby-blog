c9a14be3bcd0ed0d7701f1e5ed3304ddf5741d07
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2019-12-10 18:26:24 +0530

    Update index.tsx

--- theme/src/components/comments/index.tsx
+++ theme/src/components/comments/index.tsx
@@ -1,10 +1,17 @@
 import React, {FunctionComponent} from "react";
+import ReactDisqusComments from 'react-disqus-comments';
 
 /**
  * Placeholder which is attached under every post. Can be shadowed to
  * quickly integrate comments (like commento, Disqus, ...).
  */
-const Comments: FunctionComponent = () => <> </>;
+const Comments: FunctionComponent = () => <> <ReactDisqusComments
+                                                  shortname="leewardslope"
+                                                  identifier={post.id}
+                                                  title={post.title}
+                                                  url={post.url}
+                                                  category_id={post.category_id}
+                                               />;
 
 export default Comments;
 
